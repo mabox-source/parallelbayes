6d6dcbf65dbadcbeabfb0f62e4f5ef66069f8d86
Marc Box <marc.box@protonmail.com>
2022-04-28 17:24:50 +0100

    Write code for consensus algorithm

--- R/consensus.R
+++ R/consensus.R
@@ -44,10 +44,13 @@
 #' @param ncores an optional integer specifying the number of CPU cores to use 
 #' (see \code{\link[parallel]{makeCluster}}). The default, 1, signifies that 
 #' \code{parallel} will not be used.
+#' @param tol a number, the tolerance level for the reciprocal condition number 
+#' of the covariance matrix. If less than this, the covariances are ignored. 
+#' See details.
 #'
 #' @return A list with elements: \code{w}, list of weighting matrices used; 
 #' diagnostics \code{cov_used.partial} and \code{cov_used.pooled} (see 
-#' details); a matrix \code{theta.pooled} of pooled, weighted samples if 
+#' details); a matrix \code{theta.w.pooled} of pooled, weighted samples if 
 #' \code{return.pooled} is \code{TRUE}. If Spark is used, also field 
 #' \code{theta.w}, a Spark table similar to \code{theta} of weighted samples.
 #' 
@@ -55,17 +58,23 @@
 consensus.weights <- function(
   theta,
   type = 2,
+  return.pooled = FALSE,
   par.clust = NULL,
-  ncores = 1
+  ncores = 1,
+  cov.tol = .Machine$double.eps
 ) {
 
+  if (!(type %in% 1:2)) stop("type must be 1 or 2!")
   if (class(theta)[1] == "tbl_spark") {
     if (!require(sparklyr)) stop("sparklyr is required!")
+    Hvec <- theta %>% select(1) %>% count()
     use_spark <- TRUE
   } else {
     if (!("list" %in% class(theta))) stop("theta must be a Spark table or a list!")
+    Hvec <- sapply(theta, nrow)
     use_spark <- FALSE
   }
+  if (any(Hvec != Hvec[1])) stop("Each set of samples must have the same number of realisations!")
   if (!use_spark && !is.null(par.clust) && class(par.clust)[1] == "SOCKcluster" && require(parallel)) {
     use_parallel <- TRUE
     new_cluster <- FALSE
@@ -83,5 +92,203 @@ consensus.weights <- function(
     ncores <- 1
     use_parallel <- FALSE
   }
+
+  # First derive weights.
+
+  ctx <- list(
+    use_spark = use_spark,
+    type = type,
+    tol = tol
+  )
+  if (use_spark) {
+    spark.res <- spark_apply(theta, consensus.worker, context = ctx)
+    local.res <- as.data.frame(spark.res)
+    # Check for error flag.
+    if (any(is.na(local.res[,2]))) stop("Insufficient useable samples!") 
+    w <- split(local.res[,2], local.res[,1])
+  } else if (use_parallel) {
+    w <- parLapply(par.clust, theta, consensus.worker, context = ctx)
+    if (any(sapply(w, function(wi) {is.na(wi[,1])}))) stop("Insufficient useable samples!")
+  } else {
+    w <- lapply(theta, consensus.worker, ctx)
+    if (any(sapply(w, function(wi) {is.na(wi[,1])}))) stop("Insufficient useable samples!")
+  }
+  names(w) <- NULL
+  cov_used.partial <- sapply(w, function(wi) {wi[1] > 0})
+  w <- lapply(w, function(wi) {as.matrix(wi[,-1,drop = FALSE])})
+  w <- lapply(w, function(wi) {dimnames(wi) <- NULL; wi})
+  w.pooled <- Reduce("+", w)
+  if (type == 2) {
+    # Check reciprocal condition number. If it suggests we cannot invert w, use 
+    # the variances only as suggested by Scott et al 2016.
+    if (rcond(w.pooled) <= cov.tol) {
+      w.pooled <- diag(diag(w.pooled))
+      cov_used.pooled <- FALSE
+    } else {cov_used.pooled <- TRUE}
+  } else {cov_used.pooled <- FALSE}
+  w.pooled <- solve(w.pooled)
+
+  # Weight the samples.
+
+  if (return.pooled) {
+    if (use_spark) {
+      theta.w.spark <- spark_apply(theta,
+        function(df, context) {
+          index <- df[1,1]
+          n <- nrow(df) - 1
+          theta.w <- matrix(NA, n, ncol(df))
+          theta.w[,1] <- index
+          theta.w[,2:ncol(theta.w)] <- as.matrix(df[,2:ncol(df),drop = FALSE]) %*% context$w[[index]]
+          theta.w[,2:ncol(theta.w)] <- theta.w[,2:ncol(theta.w),drop = FALSE] %*% context$w.pooled
+          theta.w
+        },
+        context = ctx
+      )
+      if (return.pooled) {
+        theta.w.pooled <- as.data.frame(theta.w.spark)
+        theta.w.pooled <- split(theta.w.pooled[,-1,drop = FALSE], theta.w.pooled[,1])
+        theta.w.pooled <- lapply(theta.w.pooled, FUN = as.matrix)
+        theta.w.pooled <- rowSums(abind::abind(theta.w.pooled, along = 3), dims = 2)
+      }
+    } else if (use_parallel) {
+      theta.w.pooled <- rowSums(abind::abind(
+        clusterMap(
+          paar.clust,
+          fun = function(th, wi) {th %*% wi},
+          theta,
+          w,
+          SIMPLIFY = FALSE
+        ),
+        long = 3
+      ), dims = 2)
+    } else {
+      theta.w.pooled <- rowSums(abind::abind(
+        mapply(
+          FUN = function(th, wi) {th %*% wi},
+          theta,
+          w,
+          SIMPLIFY = FALSE
+        ),
+        long = 3
+      ), dims = 2)
+    }
+    dimnames(theta.w.pooled) <- NULL
+  }
+
+  out <- list(
+    w = w,
+    cov_used.partial = cov_used.partial,
+    cov_used.pooled = cov_used.pooled
+  )
+  if (use_spark) out$theta.w.spark <- theta.w.spark
+  if (return.pooled) out$theta.w.pooled <- theta.w.pooled
+  if (use_parallel && new_cluster) stopCluster(par.clust)
+}
+
+#' Compute consensus algorithm weight matrices
+#'
+#' This is a worker function for computing weight matrices for samples from 
+#' partial posteriors. If samples are held in local memory, should be applied 
+#' iteratively using a loop, but can be applied in parallel using the 
+#' \code{parallel} package or \code{sparklyr} if samples are distributed as a 
+#' Spark table.
+#'
+#' \code{context} should be a list with the following fields:
+#' \itemize{
+#' \item{\code{type}: integer, either 1 or 2, specifying the weighting type to 
+#' use. 1: use constant weighting. 2: weight samples using the sample covariance 
+#' matrices of the partial posteriors.}
+#' \item{\code{use_spark}: logical. \code{TRUE} if a Spark cluster is available.}
+#' \item{\code{tol}: the tolerance level for the reciprocal condition number of 
+#' the covariance matrix. If less than this, the covariances are ignored.}
+#' }
+#'
+#' @section References
+#' \itemize{
+#' \item{Scott, Steven L., Blocker, A.W., Bonassi, F.V., Chipman, H.A., George, E.I. and McCulloch, R.E., 2016. Bayes and big data: The consensus Monte Carlo algorithm. \emph{International Journal of Management Science and Engineering Management}, 11(2), pp.78-88.}
+#' }
+#'
+#' @param df a matrix or data.frame of samples. If using Spark the first column 
+#' should contain integers identifying the partial posterior.
+#' @param context a list of parameters common to all workers. See details.
+#'
+#' @return Consensus algorithm weighting matrix for the sample. If using Spark 
+#' the first column will contain the index of the partial posterior from which 
+#' the sample was drawn. The next column is a binary flag indicating whether 
+#' (1) or not (0) the full covariance matrix was used. The number of remaining 
+#' columns is equal to the number of rows. These columns constitute a square 
+#' matrix: the weighting matrix for consensus algorithm \code{type}.
+#' @export
+consensus.worker <- function(df, context) {
+  # On Spark the first column is the part label identifying the data shard.
+  w.part <- matrix(NA, ncol(df) - context$use_spark * 1, ncol(df) + 1)
+  # Insufficient useable samples. Return NA matrix to signal error.
+  if (nrow(df) < 2) return(w.part)
+  # The shard label.
+  if (context$use_spark) w.part[,1] <- df[1,1]
+  # Weight by sample covariance matrix.
+  if (context$type == 2) {
+    if (context$use_spark) {
+      v <- cov(df[,-1,drop = FALSE])
+    } else {
+      v <- cov(df)
+    }
+    # Check reciprocal condition number. If it suggests we cannot invert v, 
+    # ignore the covariances as suggest by Scott et al 2016.
+    is_well_conditioned <- rcond(v) > context$tol
+    if (!is_well_conditioned) v <- diag(diag(v))
+    v <- solve(v)
+
+  # Equal weighting.
+  } else {
+    v <- diag(nrow(w.part))
+    is_well_conditioned <- TRUE
+  }
+  w.part[,1 + context$use_spark * 1] <- is_well_conditioned * 1
+  w.part[,(2 + context$use_spark * 1):ncol(w.part)] <- v
+  w.part
+}
+
+#' Consensus algorithm jackknife bias correction
+#'
+#' Compute the jackknife estimate of the small sample bias that arises in the 
+#' consensus Monte Carlo algorithm. The result can be subtracted from the 
+#' consensus weighted sample to correct for this bias.
+#'
+#' @seealso \code{consensus.weights}
+#'
+#' @param theta a matrix containing the pooled samples from all partial 
+#' posteriors, weighted using the consensus algorithm. See output field 
+#' \code{theta.w.pooled} from \code{\link{consensus.weights}}.
+#' @param subsamples a list of matrix, each consisting of (unweighted) samples 
+#' of model parameters from a single partial posterior, chosen at random from 
+#' all available samples from that partial posterior and representing a 
+#' proportion \code{alpha} of all available samples. See argument \code{theta} 
+#' to \code{\link{consensus.weights}}.
+#' @param alpha a number in [0,1], the proportion of samples taken from all 
+#' available from each partial posterior to form each element of 
+#' \code{subsamples}.
+#' @param type an integer, either 1 or 2, specifying the weighting type to use. 
+#' 1: use constant weighting. 2: weight samples using the sample covariance 
+#' matrices of the partial posteriors. Must be the same as used to compute the 
+#' weighted samples in \code{theta}.
+#' 
+#' @return A list with two elements: matrix \code{theta} of bias-corrected 
+#' pooled samples, similar to the argument of the same name, and \code{bias}, 
+#' the bias correction vector.
+#' @export
+bias_correction <- function (
+  theta,
+  subsamples,
+  alpha,
+  type = 2
+) {
+  subsamples.w.pooled <- consensus.master(subsamples, type = type, return.pooled = TRUE)$theta.w.pooled
+
+  bias <- (colMeans(subsamples.w.pooled) - colMeans(theta)) * alpha / (1 - alpha)
+
+  theta <- sweep(theta, MARGIN = 2, STATS = bias, FUN = "-", check.margin = FALSE)
+
+  return(list(theta = theta, bias = bias))
 }
 
